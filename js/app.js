b2d51c89e52be1820433a218c410bcc7bdae4c05
calebcram <49875750+calebcram@users.noreply.github.com>
2021-03-17 17:23:43 -0600

    Build 3 - Changed the tracking target

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Build.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Build3.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
