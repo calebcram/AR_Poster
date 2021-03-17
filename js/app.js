94975c5371372666da1b5da7f34c1e6f4b919af5
calebcram <49875750+calebcram@users.noreply.github.com>
2021-03-17 17:55:00 -0600

    Build4 - Switched Tracking target to QR code

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Build3.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Build4.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
