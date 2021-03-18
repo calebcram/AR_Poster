ae611053520ce95b6091944ce2287264306bb9dc
calebcram <49875750+calebcram@users.noreply.github.com>
2021-03-17 19:19:58 -0600

    Build6 - Changed video from file to url

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Build5.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Build6.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
